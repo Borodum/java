76484b1738033aa21082edd07a8470f0f6cb9543
Borodum <batraz@dzesov.ru>
2022-05-05 18:59:33 +0300

    initial 3

---
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int n=1;
+    }
+}
